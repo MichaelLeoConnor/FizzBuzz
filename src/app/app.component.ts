891a336b09f2513bff833d36e5e2a73d83ed6ee0
michaelleoconnor <michael.leo.connor@protonmail.com>
2022-04-03 12:17:15 -0700

    creating fizzbuzz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fizz-buzz';
+export class AppComponent implements OnInit {
+  title: string = 'fizz-buzz';
+  public fizzBuzzArr: string[] = [];
+  
+  constructor() {}
+  
+  ngOnInit(): void {
+    for (let i: number = 1; i <= 100; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        this.fizzBuzzArr.push("FizzBuzz");
+      } 
+      else if (i % 3 == 0) {
+        this.fizzBuzzArr.push("Fizz");
+      } else {
+        this.fizzBuzzArr.push(i.toString());
+      }
+    }
+  }
 }
